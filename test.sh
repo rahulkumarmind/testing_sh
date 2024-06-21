af8c5c0550be3135409e9bd5566661aee9ebc4e6
rahulkumarmind <164403895+rahulkumarmind@users.noreply.github.com>
2024-06-21 15:05:51 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,15 @@
 WALLPAPER_URL="https://file-examples.com/storage/fed5266c9966708dcaeaea6/2017/10/file_example_JPG_100kB.jpg"
 RINGTONE_URL="https://onlinetestcase.com/wp-content/uploads/2023/06/100-KB-MP3.mp3"
 
-# # Paths to save the downloaded files
+# Paths to save the downloaded files
 WALLPAPER_PATH="/sdcard/Download/wallpaper.jpg"
 RINGTONE_PATH="/sdcard/Download/ringtone.mp3"
 
+# Ensure Termux has storage access
+termux-setup-storage
+
 # Download the wallpaper
-curl -o "/sdcard/Download/wallpaper.jpg https://file-examples.com/storage/fed5266c9966708dcaeaea6/2017/10/file_example_JPG_100kB.jpg
+curl -o "$WALLPAPER_PATH" "$WALLPAPER_URL"
 if [ $? -ne 0 ]; then
   echo "Failed to download wallpaper."
   exit 1
@@ -23,13 +26,13 @@ if [ $? -ne 0 ]; then
 fi
 
 # Set the wallpaper
-am start -a android.intent.action.SET_WALLPAPER -d file:///sdcard/Download/wallpaper.jpg
+am start -a android.intent.action.SET_WALLPAPER -d file://$WALLPAPER_PATH
 if [ $? -ne 0 ]; then
   echo "Failed to set wallpaper."
   exit 1
 fi
 
-# # Set the ringtone
+# Set the ringtone
 RINGTONE_URI=$(content insert --uri content://media/external/audio/media --bind data:text/plain:$RINGTONE_PATH --bind title:text/plain:"Custom Ringtone" --bind mime_type:text/plain:"audio/mp3" --bind is_ringtone:int:1)
 if [ $? -ne 0 ]; then
   echo "Failed to set ringtone."
